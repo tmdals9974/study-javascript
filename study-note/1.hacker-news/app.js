fab6d0fd50eb66e3c1a1a672a75679c08d4e75e9
dev_sm <tmdals9974@naver.com>
2022-12-08 23:25:41 +0900

    feat: 3-5강 학습내용
    1. 리팩토링 : DOM API 제거, 기능 함수화
    2. 라우터 구현

--- study-note/1.hacker-news/app.js
+++ study-note/1.hacker-news/app.js
@@ -12,33 +12,47 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
 
-window.addEventListener('hashchange', function() {
-  const id = location.hash.substring(1);
+  const newsList = [];
+  newsList.push('<ul>');
+  for(let i = 0; i<10; i++) {
+    newsList.push(`
+      <li>
+        <a href="#${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `);
+  }
+  newsList.push('</ul>');
   
-  const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement('h1');
-
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-
-for(let i = 0; i<10; i++) {
-  const div =document.createElement('div');
-
-  div.innerHTML = 
-  `
-  <li>
-    <a href="#${newsFeed[i].id}">
-      ${newsFeed[i].title} (${newsFeed[i].comments_count})
-    </a>
-  </li>
-  `
-
-  ul.appendChild(div.firstElementChild);
+  container.innerHTML = newsList.join("");
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+    const id = location.hash.substring(1);
+    const newsContent = getData(CONTENT_URL.replace("@id", id));
+  
+    container.innerHTML = `
+      <h1>${newsContent.title}</h1>
+      <div>
+        <a href="#">목록으로</a>
+      </div>
+    `;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
