dddea225e9a07dac479ed2103e8722747dcb7055
dev_sm <tmdals9974@naver.com>
2023-01-04 11:05:18 +0900

    feat: 6-52강 학습내용

---
+++ study-note/6.reference-dictionary/4.tool/7.restful.js
@@ -0,0 +1,8 @@
+/****************************************************************
+ * 
+ * 
+ * * Reference
+ * https://developer.mozilla.org/ko/docs/Web/HTTP/Overview
+ * https://en.wikipedia.org/wiki/Representational_state_transfer
+ * https://en.wikipedia.org/wiki/Clean_URL
+ ****************************************************************/
